c19ccd909010c6869338b8965547f8a83d0ad693
sergio-pascual <sergio.pascual@globant.com>
2022-12-15 17:35:59 -0600

    05-menu done

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,7 +4,41 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menu, setMenu] = useState(items);
+  const [category, setCategory] = useState('');
+
+  const allCategories = [...new Set(items.map( c => c.category)) ]
+
+  const filterMenuByCategory = (categoryName = '') => {
+    if(category === categoryName) return;
+
+    const filteredMenu = items.filter( ({category}) => {
+      return category === categoryName;
+    })
+
+    const newMenu = filteredMenu.length ? filteredMenu : items;
+
+    setMenu(newMenu);
+    setCategory(categoryName)
+  }
+
+  return (
+    <main>
+      <section className='section menu'>
+        <div className='title'>
+          <h2>Our menu</h2>
+          <div className='underline'></div>
+        </div>
+
+        <Menu
+          filterMenu={filterMenuByCategory}
+          options={allCategories}
+        />
+
+        <Categories items={menu}/>
+      </section>
+    </main>
+  )
 }
 
 export default App;
--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ filterMenu = () => {}, options = [] }) => {
+
+  return (
+    <div className='btn-container'>
+      <button className='filter-btn' onClick={() => filterMenu()}>All</button>
+      {
+        options.map(opt => (
+          <button
+            key={opt}
+            className='filter-btn'
+            onClick={() => filterMenu(opt)}>{opt}</button>
+        ))
+      }
+    </div>
+  )
 };
 
 export default Menu;
