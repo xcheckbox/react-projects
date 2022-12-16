7b71fb9ab6a2291c9abba431876fa09aaf922e80
sergio-pascual <sergio.pascual@globant.com>
2022-12-16 16:48:35 -0600

    09-color generator 1st part

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ type, rgb, weight, hex }) => {
+
+  const [r,g,b] = rgb;
+  const hexColor = rgbToHex(r,g,b);
+
+  return (
+    <div
+      className={`color ${(type === 'shade' ? 'color-light' : '')}`}
+      style={{ backgroundColor: `rgb(${rgb.join(',')})`}}>
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexColor}</p>
+  </div>
+  )
 }
 
 export default SingleColor
