600df64112643611f4edd7bf7b886778c1826ab5
sergio-pascual <sergio.pascual@globant.com>
2022-12-19 16:12:14 -0600

    removed unnecessary code

--- 10-grocery-bud/setup/src/hooks/useLocalStorage.js
+++ 10-grocery-bud/setup/src/hooks/useLocalStorage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useLocalStorage = (key, initialValue = '') => {
   const localInitialValue = JSON.parse(localStorage.getItem(key));
