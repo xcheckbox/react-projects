b1d929c5872cef243bfc6a328f947c9866015171
sergio-pascual <sergio.pascual@globant.com>
2022-12-19 13:46:52 -0600

    10-grocery project styles

--- 10-grocery-bud/setup/src/Alert.js
+++ 10-grocery-bud/setup/src/Alert.js
@@ -1,7 +1,21 @@
 import React, { useEffect } from 'react'
 
 const Alert = () => {
-  return <h2>alert component</h2>
+  return (
+    <>
+      <div className='alert alert-danger'>
+        Please enter value
+      </div>
+
+      <div className='alert alert-success'>
+        Item added to the list
+      </div>
+
+      <div className='alert alert-danger'>
+        Empty list
+      </div>
+    </>
+  )
 }
 
 export default Alert
--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
+import { FaEdit, FaTrash } from 'react-icons/fa'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  return (
+    <div className='section-center'>
+      <Alert />
+      <div className='grocery-form'>
+        <h3 className='title'>Grocery Bud</h3>
+
+        <form className='form-control'>
+          <input className='grocery'  placeholder='e.g. eggs' />
+          <button type='submit' className='submit-btn'>Submit</button>
+        </form>
+
+        <div className='grocery-container'>
+          <div className='grocery-item'>
+            <p className='title'>Item 1</p>
+            <div>
+              <button type='button' className='edit-btn'>
+                <FaEdit />
+              </button>
+              <button type='button' className='delete-btn'>
+                <FaTrash />
+              </button>
+            </div>
+          </div>
+
+          <button type='button' className='clear-btn'>Clear items</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
