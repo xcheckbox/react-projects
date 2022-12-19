afa9d78fd813089943fad105ea8818942278b6b6
sergio-pascual <sergio.pascual@globant.com>
2022-12-19 16:01:17 -0600

    edit action implemented

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -6,6 +6,8 @@ import { useLocalStorage } from './hooks/useLocalStorage';
 function App() {
   const [data, saveData] = useLocalStorage('store', []);
   const [inputValue, setInputValue] = useState('');
+  const [edit, setEdit] = useState(false);
+  const [selectedItem, setSelectedItem] = useState({});
   const [ alert, setAlert ] = useState({
     title: '',
     type: 'danger',
@@ -23,16 +25,29 @@ function App() {
       return;
     }
 
-    const item = {
-      id: new Date().getTime(),
-      title: inputValue,
+    if(edit) {
+      const updatedList = data.map( item => {
+        return (item.id === selectedItem.id)
+          ? {...item, title: inputValue }
+          : item
+        
+        })
+        saveData(updatedList);
+        setSelectedItem({})
+        setEdit(false);
+
+    } else {
+      const item = {
+        id: new Date().getTime(),
+        title: inputValue,
+      }
+      saveData([...data, item ]);
     }
 
-    saveData([...data, item ]);
     setInputValue('');
     
     setAlert({
-      title: 'Item added to the list',
+      title: (!edit) ? 'Item added to the list' : 'Value changed',
       show: true,
       type: 'success'
     })
@@ -56,6 +71,10 @@ function App() {
   }
 
   const editItem = (id) => {
+    const itemToEdit = data.filter( item => item.id === id);
+    setEdit(true);
+    setInputValue(itemToEdit[0].title);
+    setSelectedItem(...itemToEdit)
   }
 
   return (
@@ -65,14 +84,16 @@ function App() {
       <div className='grocery-form'>
         <h3 className='title'>Grocery Bud</h3>
 
-        <form className='form-control' onSubmit={ handleSubmit }>
+        <form className='form-control' onSubmit={handleSubmit}>
           <input
             className='grocery'
             placeholder='e.g. eggs'
             value={inputValue}
             onChange={handleInputValue}
           />
-          <button type='submit' className='submit-btn'>Submit</button>
+          <button type='submit' className='submit-btn'>
+            { !edit ? 'Submit' : 'Edit' } 
+          </button>
         </form>
 
         <List
