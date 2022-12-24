a524c7052a29a19978373e6f6396844c8929b97c
sergio-pascual <sergio.pascual@globant.com>
2022-12-24 13:06:05 -0600

    11-navbar done

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,8 +1,26 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
+import { AppContext } from './context'
 
 const Home = () => {
-  return <h2>home component</h2>
+  const { toggleModal, toggleSidebar } = useContext(AppContext);
+
+  return (
+    <main>
+      <button
+        className='sidebar-toggle'
+        onClick={toggleSidebar}
+      >
+        <FaBars />
+      </button>
+      <button
+        className='btn'
+        onClick={() => toggleModal()}
+      >
+        Show modal
+      </button>
+    </main>
+  )
 }
 
 export default Home
