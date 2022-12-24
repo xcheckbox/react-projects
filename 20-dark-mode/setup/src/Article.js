789a8ef5b5446eff24c4638dc7c8786c2ea0ac60
sergio-pascual <sergio.pascual@globant.com>
2022-12-23 23:58:17 -0600

    20-dark-mode done

--- 20-dark-mode/setup/src/Article.js
+++ 20-dark-mode/setup/src/Article.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
+const Article = ({ article }) => {
+  return (
+    <article className='post'>
+      <h2>{ article.title }</h2>
+      <div className='post-info'>
+        <span>{moment(article.date).format('dddd Do, YYYY')}</span>
+        <span>{article.length} min read</span>
+      </div>
+      <p>{article.snippet}</p>
+    </article>
+  )
 }
 
 export default Article
