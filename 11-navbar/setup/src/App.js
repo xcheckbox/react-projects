254e74a4032b6f09a5681bc8e35681fc0d5ecf92
sergio-pascual <sergio.pascual@globant.com>
2022-12-24 23:23:16 -0600

    11-navbar done

--- 11-navbar/setup/src/App.js
+++ 11-navbar/setup/src/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Navbar from './Navbar'
 function App() {
   return (
-    <>
-      <h2>navbar project setup</h2>
-    </>
+    <nav>
+      <Navbar />
+    </nav>
   )
 }
 
--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,55 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+
+  const [showNavbar, setShowNavbar] = useState(false);
+  const linksContainer = useRef(null);
+  const linksList = useRef(null);
+
+  const toggleNavbar = () => {
+    setShowNavbar(!showNavbar);
+  }
+
+  useEffect(() => {
+    if(showNavbar) {
+      linksContainer.current.style.height = '250px';
+    } else {
+      linksContainer.current.style.height = '0px';
+    }
+  }, [showNavbar])
+
+  return (
+    <div className='nav-center'>
+      <div className='nav-header'>
+        <img className='logo' src={logo} alt='Coding Addict' />
+        <button className='nav-toggle' onClick={() => toggleNavbar()}>
+          <FaBars />
+        </button>
+      </div>
+
+      <div className='links-container' ref={linksContainer}>
+        <ul className='links' ref={linksList}>
+          {
+            links.map( ({url, text, id}) => (
+              <li key={id}><a href={url}>{text}</a></li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <div className='social-icons'>
+        {
+          social.map(({id, url, icon}) => (
+            <a key={id} href={url}>{icon}</a>
+          ))
+        }
+      </div>
+    </div>
+
+  )
 }
 
 export default Navbar
