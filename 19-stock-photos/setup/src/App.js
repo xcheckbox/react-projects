d2506a8594cde5779863d5a573f2bf0de975145a
sergio-pascual <sergio.pascual@globant.com>
2022-12-27 00:23:05 -0600

    displating photos from unsplash

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,12 +1,60 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+import { PhotoAdapter } from './photo-adapter'
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
-  return <h2>stock photos starter</h2>
+
+  console.log(process.env.REACT_APP_ACCESS_KEY)
+  
+  const [ data, setData ] = useState([]);
+  const [ loading, setLoading ] = useState(false);
+
+  useEffect(() => {
+    fetchData(`${mainUrl}${clientID}`);
+  }, []);
+
+
+  const fetchData = async (url) => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(PhotoAdapter(data));
+    } catch (error) {
+      setData(null);
+    }
+    setLoading(false);
+  }
+
+  return (
+    <>
+    <div className='search'>
+      <form className='search-form'>
+        <input
+          placeholder='search'
+          className='form-input'
+        />
+        <button type='submit' className='submit-btn'>
+          <FaSearch />
+        </button>
+      </form>
+    </div>
+    <div className='photos'>
+      <div className='photos-center'>
+        {
+          data?.map( photo => {
+            return (<Photo photo={photo} />)
+          })
+        }
+      </div>
+    </div>
+    </>
+  )
 }
 
 export default App
--- 19-stock-photos/setup/src/Photo.js
+++ 19-stock-photos/setup/src/Photo.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
+const Photo = ({ photo }) => {
+  const { image, name, likes, user, userImg, userUrl } = photo;
+
+  return (
+    <div className='photo'>
+      <img src={image} alt={name} />
+      <div className='photo-info'>
+        <div>
+          <h4>{ user }</h4>
+          <p>{ likes } likes</p>
+        </div>
+        <a href={userUrl} rel='noreferrer noopener' target='_blank'>
+          <img className='user-img' src={userImg} alt={user} />
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Photo
