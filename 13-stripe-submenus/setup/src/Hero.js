e8bd789519c6d4c907d5f583a41c7300c7f3d0c2
sergio-pascual <sergio.pascual@globant.com>
2022-12-29 15:25:47 -0600

    package-json added

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -2,7 +2,23 @@ import React from 'react'
 import phoneImg from './images/phone.svg'
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  return (
+    <div className='hero'>
+      <div className='hero-center'>
+        <div className='hero-info'>
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+          Millions of companies of all sizes—from startups to Fortune 500s—use Stripe’s software and APIs to accept payments, send payouts, and manage their businesses online.
+          </p>
+          <button type='button' className='btn'>Start now</button>
+        </div>
+
+        <div className='hero-images'>
+          <img className='phone-img' src={phoneImg} alt='stripe-payments' />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Hero
